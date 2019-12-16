e068a3a2cfb3799e803bf5f4916f110a345f6b23
jacob-oreilly <jakeor45@gmail.com>
2019-12-15 21:37:14 -0600

    Adding the python backend.

---
+++ BailSafe_BackEnd/buttontest.py
@@ -0,0 +1,24 @@
+## Import GPIO Library
+import RPi.GPIO as GPIO
+
+
+## Setup Pins
+GPIO.setmode(GPIO.BOARD)
+inputPin = 7
+GPIO.setup(inputPin, GPIO.IN)
+
+##  Need try and finally so if the program crashes
+try:
+    
+    while True:
+        if GPIO.input(inputPin)== 0:
+            print("No Rider")
+        else:
+            print("On Seat")
+
+finally:
+    ## Need to close pins
+    GPIO.cleanup()
+    
+    
+    
